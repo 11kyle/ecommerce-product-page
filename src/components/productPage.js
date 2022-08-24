0e3e726aecbd62b5e993b8ee01b42e03253a85d7
11kyle <11kyle.johnson@gmail.com>
2022-08-24 13:13:37 -0500

    Replaced sections tags with div tags due to them not having heading tags

--- src/components/productPage.js
+++ src/components/productPage.js
@@ -104,7 +104,7 @@ export default function ProductPage({ setItems }) {
 
   return (
     <main className="md:grid md:grid-cols-2 md:gap-x-6 lg:gap-x-20 md:mt-20 md:mx-10 mb-40">
-      <section>
+      <div>
         <div className="relative">
             <img className="md:rounded-2xl pointer-events-none md:pointer-events-auto md:cursor-pointer" src={product.images[active].main} alt="shoe" onClick={handleOpen} />
 
@@ -126,8 +126,8 @@ export default function ProductPage({ setItems }) {
           })}
 
         </div>
-      </section>
-      <section className="mx-6  md:place-self-center">
+      </div>
+      <div className="mx-6  md:place-self-center">
         <div>
           <h2 className="uppercase text-sm font-bold text-hslOrange my-4">Sneaker Company</h2>
           <h1 className="text-3xl font-bold md:text-4xl">{product.name}</h1>
@@ -154,10 +154,10 @@ export default function ProductPage({ setItems }) {
             <svg width="22" height="20" xmlns="http://www.w3.org/2000/svg">
               <path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fill="#ffffff"/>
             </svg>
-            <p className="font-bold">Add to cart</p>
+            <span className="font-bold">Add to cart</span>
           </button>
         </div>
-      </section>
+      </div>
       
       <Modal 
         active={active}
